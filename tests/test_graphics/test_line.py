8761f04d0af55706fbafc1e07df23ffeb5edd852
gram <git@orsinium.dev>
2025-11-27 16:13:01 +0100

    line tests

--- tests/test_graphics/test_line.py
+++ tests/test_graphics/test_line.py
@@ -1,23 +1,162 @@
-from firefly_test import Color
 from ..manager import Manager
 
 
-def test_point_basic(manager: Manager) -> None:
+def test_line_w1_hor(manager: Manager) -> None:
     manager.build_and_render(
-        boot='DrawPoint(P(12, 14), ColorRed)',
+        boot='DrawLine(P(12, 14), P(17, 14), L(ColorRed, 1))',
     )
-    assert manager.app.frame.at(12, 14) == Color.RED
-    sub = manager.app.frame.get_sub(x=11, y=13)
+    sub = manager.app.frame.get_sub(x=11, y=12)
     sub.assert_match("""
-        KKK
-        KRK
-        KKK
+        ●●●●●●●●
+        ●●●●●●●●
+        ●RRRRRR●
+        ●●●●●●●●
     """)
 
 
-def test_point_out_of_bounds(manager: Manager) -> None:
+def test_line_w1_hor_inverse(manager: Manager) -> None:
     manager.build_and_render(
-        boot='DrawPoint(P(260, 14), ColorRed)',
+        boot='DrawLine(P(17, 14), P(12, 14), L(ColorRed, 1))',
     )
-    for color in manager.app.frame:
-        assert color == Color.BLACK
+    sub = manager.app.frame.get_sub(x=11, y=12)
+    sub.assert_match("""
+        ●●●●●●●●
+        ●●●●●●●●
+        ●RRRRRR●
+        ●●●●●●●●
+    """)
+
+
+def test_line_w2_hor(manager: Manager) -> None:
+    manager.build_and_render(
+        boot='DrawLine(P(12, 14), P(17, 14), L(ColorRed, 2))',
+    )
+    sub = manager.app.frame.get_sub(x=11, y=12)
+    sub.assert_match("""
+        ●●●●●●●●
+        ●RRRRRR●
+        ●RRRRRR●
+        ●●●●●●●●
+    """)
+
+
+def test_line_w2_hor_inverse(manager: Manager) -> None:
+    manager.build_and_render(
+        boot='DrawLine(P(17, 14), P(12, 14), L(ColorRed, 2))',
+    )
+    sub = manager.app.frame.get_sub(x=11, y=12)
+    # The order of points when drawing the line affects
+    # on which side the "extra" pixels will be drawn
+    # when the line width is odd.
+    sub.assert_match("""
+        ●●●●●●●●
+        ●●●●●●●●
+        ●RRRRRR●
+        ●RRRRRR●
+        ●●●●●●●●
+    """)
+
+
+def test_line_w3_hor(manager: Manager) -> None:
+    manager.build_and_render(
+        boot='DrawLine(P(12, 14), P(17, 14), L(ColorRed, 3))',
+    )
+    sub = manager.app.frame.get_sub(x=11, y=12)
+    sub.assert_match("""
+        ●●●●●●●●
+        ●RRRRRR●
+        ●RRRRRR●
+        ●RRRRRR●
+        ●●●●●●●●
+    """)
+
+
+def test_line_w1_vert(manager: Manager) -> None:
+    manager.build_and_render(
+        boot='DrawLine(P(12, 14), P(12, 17), L(ColorRed, 1))',
+    )
+    sub = manager.app.frame.get_sub(x=11, y=13)
+    sub.assert_match("""
+        ●●●●
+        ●R●●
+        ●R●●
+        ●R●●
+        ●R●●
+        ●●●●
+    """)
+
+
+def test_line_w2_vert(manager: Manager) -> None:
+    manager.build_and_render(
+        boot='DrawLine(P(12, 14), P(12, 17), L(ColorRed, 2))',
+    )
+    sub = manager.app.frame.get_sub(x=11, y=13)
+    sub.assert_match("""
+        ●●●●
+        ●RR●
+        ●RR●
+        ●RR●
+        ●RR●
+        ●●●●
+    """)
+
+
+def test_line_w1_tl2br_45deg(manager: Manager) -> None:
+    """Draw a 1-pixel wide line from top-left to bottom right with 45 degrees incline.
+    """
+    manager.build_and_render(
+        boot='DrawLine(P(12, 14), P(15, 17), L(ColorRed, 1))',
+    )
+    sub = manager.app.frame.get_sub(x=11, y=13)
+    sub.assert_match("""
+        ●●●●●●
+        ●R●●●●
+        ●●R●●●
+        ●●●R●●
+        ●●●●R●
+        ●●●●●●
+    """)
+
+
+def test_line_w2_tl2br_45deg(manager: Manager) -> None:
+    """Draw a 2-pixel wide line from top-left to bottom right with 45 degrees incline.
+    """
+    manager.build_and_render(
+        boot='DrawLine(P(12, 14), P(15, 17), L(ColorRed, 2))',
+    )
+    sub = manager.app.frame.get_sub(x=11, y=13)
+    # Interesting!
+    sub.assert_match("""
+        ●●●●●●●
+        ●RR●●●●
+        ●●RR●●●
+        ●●●RR●●
+        ●●●●R●●
+        ●●●●●●●
+    """)
+
+
+def test_line_w1_identity(manager: Manager) -> None:
+    manager.build_and_render(
+        boot='DrawLine(P(12, 14), P(12, 14), L(ColorRed, 1))',
+    )
+    sub = manager.app.frame.get_sub(x=11, y=12)
+    sub.assert_match("""
+        ●●●
+        ●●●
+        ●R●
+        ●●●
+    """)
+
+
+def test_line_w2_identity(manager: Manager) -> None:
+    manager.build_and_render(
+        boot='DrawLine(P(12, 14), P(12, 14), L(ColorRed, 2))',
+    )
+    sub = manager.app.frame.get_sub(x=11, y=12)
+    sub.assert_match("""
+        ●●●
+        ●R●
+        ●R●
+        ●●●
+    """)
--- tests/test_graphics/test_point.py
+++ tests/test_graphics/test_point.py
@@ -1,19 +1,54 @@
-from pathlib import Path
-
 from firefly_test import Color
 from ..manager import Manager
 
 
-def test_point_1(tmp_path: Path) -> None:
-    manager = Manager(tmp_path)
-    manager.build(boot='DrawPoint(P(12, 14), ColorRed)')
-    manager.app.start()
-    manager.app.update()
-    manager.app.update()
+def test_point_basic(manager: Manager) -> None:
+    manager.build_and_render(
+        boot='DrawPoint(P(12, 14), ColorRed)',
+    )
     assert manager.app.frame.at(12, 14) == Color.RED
     sub = manager.app.frame.get_sub(x=11, y=13)
     sub.assert_match("""
-        KKK
-        KRK
-        KKK
+        ●●●
+        ●R●
+        ●●●
+    """)
+
+
+def test_point_out_of_bounds(manager: Manager) -> None:
+    manager.build_and_render(
+        boot='DrawPoint(P(260, 14), ColorRed)',
+    )
+    for color in manager.app.frame:
+        assert color == Color.BLACK
+
+
+def test_point_colors(manager: Manager) -> None:
+    manager.build_and_render(
+        boot="""
+            DrawPoint(P(12, 14), ColorBlack)
+            DrawPoint(P(13, 14), ColorPurple)
+            DrawPoint(P(14, 14), ColorRed)
+            DrawPoint(P(15, 14), ColorOrange)
+            DrawPoint(P(16, 14), ColorYellow)
+            DrawPoint(P(17, 14), ColorLightGreen)
+            DrawPoint(P(18, 14), ColorGreen)
+            DrawPoint(P(19, 14), ColorDarkGreen)
+
+            DrawPoint(P(12, 15), ColorDarkBlue)
+            DrawPoint(P(13, 15), ColorBlue)
+            DrawPoint(P(14, 15), ColorLightBlue)
+            DrawPoint(P(15, 15), ColorCyan)
+            DrawPoint(P(16, 15), ColorWhite)
+            DrawPoint(P(17, 15), ColorLightGray)
+            DrawPoint(P(18, 15), ColorGray)
+            DrawPoint(P(19, 15), ColorDarkGray)
+        """,
+    )
+    sub = manager.app.frame.get_sub(x=11, y=13)
+    sub.assert_match("""
+        ●●●●●●●●●●
+        ●KPROYgGD●
+        ●dBbCW◔◑◕●
+        ●●●●●●●●●●
     """)
