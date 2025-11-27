c031b674b2dc0b35381df8e4ae0cfe7b92a65a9f
gram <git@orsinium.dev>
2025-11-27 17:42:06 +0100

    test unreachable, GetName

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -1,4 +1,11 @@
+import pytest
 from .manager import Manager
+
+
+def test_unreachable(manager: Manager) -> None:
+    msg = "error calling boot: wasm `unreachable` instruction executed."
+    with pytest.raises(RuntimeError, match=msg):
+        manager.build_and_render(boot='panic("")')
 
 
 def test_log_debug(manager: Manager) -> None:
@@ -8,6 +15,18 @@ def test_log_debug(manager: Manager) -> None:
 def test_get_random(manager: Manager) -> None:
     manager.build_and_render(boot="""
         if GetRandom() == GetRandom() {
+            panic("")
+        }
+    """)
+
+
+def test_get_name(manager: Manager) -> None:
+    manager.build_and_render(boot="""
+        name := GetName(GetMe())
+        if len(name) < 4 {
+            panic("")
+        }
+        if len(name) > 16 {
             panic("")
         }
     """)
