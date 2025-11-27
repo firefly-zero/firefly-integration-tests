b7b181cfd4e403f475829d8f345e362cfe6654ea
gram <git@orsinium.dev>
2025-11-27 17:37:22 +0100

    test GetRandom

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -2,5 +2,12 @@ from .manager import Manager
 
 
 def test_log_debug(manager: Manager) -> None:
-    manager.build(boot='LogDebug("hello")')
-    manager.app.start()
+    manager.build_and_render(boot='LogDebug("hello")')
+
+
+def test_get_random(manager: Manager) -> None:
+    manager.build_and_render(boot="""
+        if GetRandom() == GetRandom() {
+            panic("")
+        }
+    """)
