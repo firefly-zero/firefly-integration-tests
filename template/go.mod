262ab2031499b6d52ec15eb162d666932cb099e5
gram <git@orsinium.dev>
2025-11-28 17:52:39 +0100

    update SDK, test more FS funcs

--- template/go.mod
+++ template/go.mod
@@ -2,6 +2,6 @@ module test
 
 go 1.25.4
 
-require github.com/firefly-zero/firefly-go v0.9.4
+require github.com/firefly-zero/firefly-go v0.9.5
 
 require github.com/orsinium-labs/tinymath v1.0.0 // indirect
--- tests/test_fs.py
+++ tests/test_fs.py
@@ -1,12 +1,34 @@
 from .manager import Manager
 
 
-def test_file_roundtrip(manager: Manager) -> None:
+def test_load_file(manager: Manager) -> None:
     manager.build_and_render(
         boot="""
             old := "hello"
             DumpFile("fname", []byte(old))
             new := LoadFile("fname", nil)
             assert(string(new.Raw) == old)
+        """,
+    )
+
+
+def test_get_file_size(manager: Manager) -> None:
+    manager.build_and_render(
+        boot="""
+            assert(GetFileSize("fname") == 0)
+            assert(!FileExists("fname"))
+            DumpFile("fname", []byte("hello"))
+            assert(GetFileSize("fname") == 5)
+            assert(FileExists("fname"))
+        """,
+    )
+
+
+def test_remove_file(manager: Manager) -> None:
+    manager.build_and_render(
+        boot="""
+            DumpFile("fname", []byte("hello"))
+            RemoveFile("fname")
+            assert(!FileExists("fname"))
         """,
     )
